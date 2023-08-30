acce7233798fbfdd34d6dac200d5834e48d9a801
Tegue <teguemcnair@gmail.com>
2023-08-30 18:30:02 -0500

    master

---
+++ configuration.nix
@@ -0,0 +1,359 @@
+# First run sudo nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+
+{ 
+  config, 
+  pkgs, 
+  lib,
+  ...
+}:
+
+#let
+#  home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/master.tar.gz";
+#in
+  
+{
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+    #(import "${home-manager}/nixos")
+    <home-manager/nixos>
+  ];
+
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  };
+
+  home-manager.users.bunny = {
+    # The home.stateVersion option does not have a default and must be set
+    home.stateVersion = "23.05";
+    # Here goes the rest of your home-manager config, e.g. home.packages = [ pkgs.foo ];
+    
+    # Add Firefox GNOME theme directory
+    home.file."firefox-gnome-theme" = {
+      target = ".mozilla/firefox/default/chrome/firefox-gnome-theme";
+      source = (fetchTarball "https://github.com/rafaelmardojai/firefox-gnome-theme/archive/master.tar.gz");
+    };
+    
+    dconf.settings = {
+      "org/gnome/desktop/interface" = {
+        color-scheme = "prefer-dark";
+      };
+    };
+    
+    programs.firefox = {
+    enable = true;
+    profiles = {
+      default = {
+        isDefault = true;
+        name = "Default";
+        settings = {
+          # https://github.com/yokoffing/Betterfox
+          "extensions.activeThemeID" = "firefox-compact-dark@mozilla.org";
+          "dom.security.https_only_mode" = true;
+          "dom.security.https_only_mode_ever_enabled" = true;
+          "privacy.trackingprotection.enabled" = true;
+          "app.update.suppressPrompts" = true;
+          "browser.privatebrowsing.vpnpromourl" = "";
+          "browser.shell.checkDefaultBrowser" = false;
+          "browser.newtabpage.activity-stream.asrouter.userprefs.cfr.addons" = false;
+          "browser.newtabpage.activity-stream.asrouter.userprefs.cfr.features" = false;
+          "browser.preferences.moreFromMozilla" = false;
+          "browser.tabs.tabmanager.enabled" = false;
+          "full-screen-api.transition-duration.enter" = "0 0";
+          "full-screen-api.transition-duration.leave" = "0 0";
+          "full-screen-api.warning.delay" = -1;
+          "full-screen-api.warning.timeout" = 0;
+          "browser.aboutwelcome.enabled" = false;
+          "findbar.highlightAll" = true;
+          "browser.urlbar.suggest.engines" = false;
+          "browser.urlbar.suggest.topsites" = false;
+          "browser.urlbar.suggest.calculator" = true;
+          "browser.urlbar.unitConversion.enabled" = true;
+          "browser.newtabpage.activity-stream.feeds.topsites" = false;
+          "browser.newtabpage.activity-stream.feeds.section.topstories" = false;
+          "extensions.pocket.enabled" = false;
+          "browser.download.useDownloadDir" = false;
+          "browser.download.manager.addToRecentDocs" = false;
+          "browser.download.always_ask_before_handling_new_types" = true;
+          "browser.download.autohideButton" = false;
+          "dom.disable_window_move_resize" = true;
+          "layout.css.has-selector.enabled" = true;
+          "browser.contentblocking.category" = "standard";
+          "privacy.query_stripping.enabled" = true;
+          "privacy.query_stripping.enabled.pbmode" = true;
+          "urlclassifier.trackingSkipURLs" = "*.reddit.com, *.twitter.com, *.twimg.com, *.tiktok.com";
+          "urlclassifier.features.socialtracking.skipURLs" = "*.instagram.com, *.twitter.com, *.twimg.com";
+          "fission.autostart" = true;
+          "security.sandbox.gpu.level" = 1;
+          "network.cookie.cookieBehavior" = 5;
+          "browser.contentblocking.reject-and-isolate-cookies.preferences.ui.enabled" = true;
+          "privacy.partition.network_state" = true;
+          "privacy.partition.serviceWorkers" = true;
+          "privacy.partition.always_partition_third_party_non_cookie_storage" = true;
+          "privacy.partition.always_partition_third_party_non_cookie_storage.exempt_sessionstorage" = false;
+          "extensions.webcompat.enable_shims" = true;
+          "privacy.purge_trackers.enabled" = true;
+          "browser.send_pings" = false;
+          "privacy.globalprivacycontrol.enabled" = true;
+          "privacy.globalprivacycontrol.functionality.enabled" = true;
+
+          # For Firefox GNOME theme:
+          "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+          "browser.tabs.drawInTitlebar" = true;
+          "svg.context-properties.content.enabled" = true;
+        };
+        userChrome = ''
+          @import "firefox-gnome-theme/userChrome.css";
+          @import "firefox-gnome-theme/theme/colors/dark.css"; 
+        '';
+      };
+    };
+  };
+  };
+
+  # Aliases
+  environment.interactiveShellInit = ''
+    alias proton='proton-call -r'
+  '';
+
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+
+  networking.hostName = "NixOS"; # Define your hostname.
+  networking.wireless.enable = false;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "America/Chicago";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+  services.xserver.displayManager.gdm.wayland = false;
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "";
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+
+  services.avahi = {
+    nssmdns = true;
+    enable = true;
+    ipv4 = true;
+    ipv6 = true;
+    publish = {
+      enable = true;
+      addresses = true;
+      workstation = true;
+    };
+  };
+
+
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.bunny = {
+    isNormalUser = true;
+    description = "bunny";
+    extraGroups = [ "networkmanager" "wheel" "libvirtd" "docker" ];
+    packages = with pkgs; [
+      # Single User Packages
+    ];
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  #tilp = pkgs.callPackage ./tilp.nix { };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  #  wget
+    nano
+    firefox-bin
+    virt-manager
+    qemu_kvm
+    vscode
+    flatpak
+    vlc
+    gnome-photos
+    gnome.gnome-screenshot
+    gnome-console
+    gnome.gnome-system-monitor
+    gparted
+    gnomeExtensions.clipboard-indicator
+    gnomeExtensions.appindicator
+    gnome.gnome-themes-extra
+    git
+    appimage-run
+    gnome.nautilus
+    discord
+    libvirt
+    yt-dlp
+    ffmpeg
+    home-manager
+    wget
+    curl
+    nssmdns
+    citra-canary
+    steam
+    pmutils
+    p7zip
+    thunderbird
+    gnome.gnome-remote-desktop
+    spotify
+    hashcat
+    pkgs.gnome3.gnome-tweaks
+    libreoffice
+    python3Full
+    dotnet-sdk
+    python3
+    headsetcontrol
+    gnomeExtensions.headsetcontrol
+    distrobox
+    gimp
+    docker
+    shotcut
+    tor-browser-bundle-bin
+    security.pki.certificateFiles
+    
+    # Local derivations
+    (callPackage ./tilp.nix { })
+  ];
+
+  # Enable Software
+  virtualisation.docker.enable = true;
+  services.flatpak.enable = true;
+
+  # Virtual Machines
+  virtualisation.libvirtd.enable = true;
+  programs.dconf.enable = true;
+
+  # Font Config
+  fonts = {
+    enableDefaultPackages = true;
+    fontDir.enable = true;
+    fontconfig = {
+      enable = true;
+      defaultFonts = {
+        emoji = [ "Noto Color Emoji" ];
+        monospace = [
+          "Noto Sans Mono"
+          "Noto Color Emoji"
+          "emoji"
+        ];
+        sansSerif = [
+          "Noto Sans"
+          "Noto Color Emoji"
+          "emoji"
+        ];
+        serif = [
+          "Noto Serif"
+          "Noto Color Emoji"
+          "emoji"
+        ];
+      };
+    };
+    packages = with pkgs; [
+      noto-fonts
+      noto-fonts-cjk
+      noto-fonts-emoji
+    ];
+  };
+
+  # Get rid of most default packages
+  environment.gnome.excludePackages = with pkgs.gnome; [ 
+    baobab 
+    epiphany 
+    simple-scan 
+    yelp evince 
+    gnome-calculator 
+    gnome-calendar 
+    gnome-characters 
+    gnome-clocks 
+    gnome-contacts 
+    gnome-font-viewer 
+    gnome-logs 
+    gnome-maps 
+    gnome-music 
+    gnome-weather 
+    gnome-disk-utility 
+    pkgs.gnome-tour 
+    cheese 
+    geary 
+    gnome-keyring
+    gnome-software
+  ];
+  services.xserver.excludePackages = [ pkgs.xterm ];
+  services.xserver.desktopManager.xterm.enable = false;
+
+  services.udev.packages = with pkgs; [ gnome.gnome-settings-daemon ];
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.05"; # Did you read the comment?
+
+}

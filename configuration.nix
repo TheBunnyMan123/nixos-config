dfff1a21d6a7e997d55d270c3575188905d6ccae
TheKillerBunny <tegguemcnair+git@gmail.com>
2023-09-09 17:34:08 -0500

    Fix hardware-configuration

--- configuration.nix
+++ configuration.nix
@@ -13,11 +13,6 @@
     '';
   };
 
-  imports = [
-    # Include the results of the hardware scan.
-    ./hardware-configuration.nix
-  ];
-
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";

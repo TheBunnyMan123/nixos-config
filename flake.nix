55697a39ad07da73cd4a8bd44e5f0103083e61d0
TheKillerBunny <tegguemcnair+git@gmail.com>
2023-09-09 17:30:28 -0500

    Seperate files

--- flake.nix
+++ flake.nix
@@ -10,11 +10,34 @@
   outputs = { self, nixpkgs, home-manager }: {
     
     nixosConfigurations = {  
-      NixOS = nixpkgs.lib.nixosSystem {
+      NixOS-Desktop = nixpkgs.lib.nixosSystem {
           
         system = "x86_64-linux"; 
         modules = [
           ./configuration.nix
+          ./host-specific/desktop/hardware-configuration.nix
+          ./services.nix
+          ./packages.nix
+          ./host-specific/nvidia-drivers.nix
+
+          # User Specific
+          home-manager.nixosModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.bunny = import ./bunny/home.nix;
+          }
+
+        ];
+      };
+
+      NixOS-Laptop = nixpkgs.lib.nixosSystem {
+          
+        system = "x86_64-linux"; 
+        modules = [
+          ./configuration.nix
+          ./host-specific/laptop/hardware-configuration.nix
+          ./services.nix
+          ./packages.nix
           
           # User Specific
           home-manager.nixosModules.home-manager {

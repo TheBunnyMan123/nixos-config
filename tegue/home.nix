e360ff41da1b2829c4952931dc4a49f1cd81930c
TheKillerBunny <tegguemcnair+git@gmail.com>
2023-09-25 01:28:39 -0500

    Add gnome-tweaks settings to home.nix

--- tegue/home.nix
+++ tegue/home.nix
@@ -9,9 +9,53 @@
   # Here goes the rest of your home-manager config, e.g. home.packages = [ pkgs.foo ];
       
   dconf.settings = {
+    "org/gnome/mutter" = {
+      edge-tiling = true;
+      overlay-key = "Super_L";
+      attach-modal-dialogs = false;
+      center-new-windows = false;
+    };
+    "org/gnome/input-sources" = {
+      show-all-sources = false;
+    };
+    "org/gnome/desktop/calendar" = {
+      show-weekdate = false;
+    };
+    "org/gnome/desktop/wm/prefrences" = {
+      action-double-click-titlebar = "toggle-maximize";
+      action-middle-click-titlebar = "minimize";
+      action-right-click-titlebar = "menu";
+      button-layout = "appmenu:close";
+      titlebar-font = "Noto Sans Semi-Bold 11";
+      resize-with-right-button = false;
+      mouse-button-modifier = "<Super>";
+      focus-mode = "click";
+    };
+    "org/gnome/desktop/peripherals/touchpad" = {
+      disable-while-typing = false;
+      click-method = "fingers";
+    };
+    "org/gnome/desktop/peripherals/mouse" = {
+      accel-profile = "adaptive";
+    };
     "org/gnome/desktop/interface" = {
       color-scheme = "prefer-dark";
       enable-hot-corners = true;
+      gtk-enable-primary-paste = false;
+      locate-pointer = true;
+      gtk-key-theme = "Default";
+      document-font-name = "Noto Sans 11";
+      font-name = "Noto Sans 11";
+      monospace-font-name = "Source Code Pro 10";
+      text-scaling-factor = 1.0;
+      font-hinting = "slight";
+      font-antialiasing = "grayscale";
+      icon-theme = "Adwaita";
+      cursor-theme = "Adwaita";
+      gtk-theme = "Adwaita-dark";
+      clock-show-weekday = true;
+      clock-show-date = true;
+      clock-show-seconds = false;
     };
     "org/gnome/desktop/notifications" = {
       show-banners = true;
@@ -25,22 +69,29 @@
       picture-uri-dark = "file:///etc/nixos/NixOS_1440p.png";
       primary-color = "#000000000000";
       secondary-color = "#000000000000";
-    };
-    "org/gnome/mutter" = {
-      edge-tiling = true;
+      picture-options = "zoom";
     };
     "org/gnome/desktop/screensaver" = {
       lock-enabled = true;
       lock-delay = 0;
+      picture-options = "zoom";
     };
     "org/gnome/desktop/session" = {
       idle-delay = 300;
     };
-    "org/gnome/settings-daemon/plugins/power" = {
+
+    "settings-daemon/plugins/power" = {
       sleep-inactive-ac-type = "nothing";
       sleep-inactive-battery-type = "nothing";
+      power-button-action = "hibernate";
     };
-    "org/gnome/shell/extensions/HeadsetControl" = {
+
+    "shell" = {
+      disabled-extensions = ["workspace-indicator@gnome-shell-extensions.gcampax.github.com" "windowsNavigator@gnome-shell-extensions.gcampax.github.com" "window-list@gnome-shell-extensions.gcampax.github.com" "user-theme@gnome-shell-extensions.gcampax.github.com" "screenshot-window-sizer@gnome-shell-extensions.gcampax.github.com" "places-menu@gnome-shell-extensions.gcampax.github.com" "native-window-placement@gnome-shell-extensions.gcampax.github.com" "launch-new-instance@gnome-shell-extensions.gcampax.github.com" "auto-move-windows@gnome-shell-extensions.gcampax.github.com"];
+      enabled-extensions = ["drive-menu@gnome-shell-extensions.gcampax.github.com" "HeadsetControl@lauinger-clan.de" "clipboard-indicator@tudmotu.com" "apps-menu@gnome-shell-extensions.gcampax.github.com" "appindicatorsupport@rgcjonas.gmail.com"];
+    };
+    
+    "shell/extensions/HeadsetControl" =  {
       headsetcontrol-executable = "headsetcontrol";
       option-battery = "-b";
       option-capabilities = "-?";
@@ -50,18 +101,13 @@
       option-rotate-mute = "-r";
       option-sidetone = "-s";
       option-voice = "-v";
-      show-systemindicator = true;
-      use-notifications = false;
-      use-logging = true;
-      use-colors = true;
-      color-batteryhigh = "rgb(0,255,0)";
-      color-batterymedium = "rgb(255,255,0)";
+      ohow-systemindicator = true;
+      ose-notifications = false;
+      ose-logging = true;
+      ose-colors = true;
+      oolor-batteryhigh = "rgb(0,255,0)";
+      oolor-batterymedium = "rgb(255,255,0)";
       color-batterylow = "rgb(255,0,0)";
     };
-    "org/gnome/shell" = {
-      disabled-extensions = ["workspace-indicator@gnome-shell-extensions.gcampax.github.com" "windowsNavigator@gnome-shell-extensions.gcampax.github.com" "window-list@gnome-shell-extensions.gcampax.github.com" "user-theme@gnome-shell-extensions.gcampax.github.com" "screenshot-window-sizer@gnome-shell-extensions.gcampax.github.com" "places-menu@gnome-shell-extensions.gcampax.github.com" "native-window-placement@gnome-shell-extensions.gcampax.github.com" "launch-new-instance@gnome-shell-extensions.gcampax.github.com" "auto-move-windows@gnome-shell-extensions.gcampax.github.com"];
-      enabled-extensions = ["drive-menu@gnome-shell-extensions.gcampax.github.com" "HeadsetControl@lauinger-clan.de" "clipboard-indicator@tudmotu.com" "apps-menu@gnome-shell-extensions.gcampax.github.com" "appindicatorsupport@rgcjonas.gmail.com"];
-    };
   };
-      
 }

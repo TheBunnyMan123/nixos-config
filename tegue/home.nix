a02be7e55c1455a6059c3803dc7f37456613794d
Tegue <teguemcnair@gmail.com>
2023-10-03 20:35:40 -0500

    e

--- tegue/home.nix
+++ tegue/home.nix
@@ -267,14 +267,14 @@
       enable-wide-handle=false;
       middle-click-close=true;
       sidebar-on-right=false;
-      tab-position='top';
+      tab-position="top";
       terminal-title-style="small";
     };
     "com/gexperts/Tilix/profiles/2b7c4080-0ddd-46c5-8f23-563fd3ba789d" = {
       background-transparency-percent=27;
       dim-transparency-percent=0;
       scroll-on-output=false;
-      visible-name='Default';
+      visible-name="Default";
     };
   };
 }

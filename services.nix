da99a32065714698672a144106bdd5891fb91aa6
Tegue <teguemcnair@gmail.com>
2023-10-04 21:56:24 -0500

    better grub theme

--- services.nix
+++ services.nix
@@ -80,6 +80,17 @@
 #    efiInstallAsRemovable = true; # in case canTouchEfiVariables doesn't work for your system
     device = "nodev"; #/dev/sda";
 
+    theme = pkgs.stdenv.mkDerivation {
+      pname = "distro-grub-themes";
+      version = "3.1";
+      src = pkgs.fetchFromGitHub {
+        owner = "AdisonCavani";
+        repo = "distro-grub-themes";
+        rev = "v3.1";
+        hash = "sha256-ZcoGbbOMDDwjLhsvs77C7G7vINQnprdfI37a9ccrmPs=";
+      };
+      installPhase = "cp -r customize/nixos $out";
+    };
     extraEntries = ''
       menuentry "Reboot" {
         reboot

14a80dc0e3942f82236a55d55ab74cd30b49d78d
TheKillerBunny <tegguemcnair+git@gmail.com>
2023-10-11 20:17:35 -0500

    Add icons directly to files

--- packages/calculator.nix
+++ packages/calculator.nix
@@ -1,12 +1,26 @@
 {
   fetchFromGitHub,
   buildDotnetModule,
+  copyDesktopItems,
   pkgs
 }:
  
  buildDotnetModule rec {
   name = "calculator";
   version = "1.0.2";
+
+  desktopItems = [
+    (
+      pkgs.makeDesktopItem {
+        name = "calculator";
+        desktopName = "Calculator";
+        exec = "Calculator";
+        icon = "com.thekillerbunny.calculator";
+        categories = [ "Utility" "X-Accessory"];
+        terminal = false;
+      }
+    )
+  ];
 
   src = fetchFromGitHub {
     owner = "TheBunnyMan123";
@@ -15,9 +29,16 @@
     sha256 = "sha256-2igzSEE8wqdYW/4CR1JpWJY4f+WcMtz0OtFiZUtr8t4=";
   };
 
+  nativeBuildInputs = [ copyDesktopItems ];
+
   runtimeDeps = [ pkgs.fontconfig pkgs.xorg.libX11 pkgs.xorg.libICE pkgs.xorg.libSM ];
 
   nugetDeps = ./deps-calc.nix;
   projectFile = "Calculator.csproj";
+
+  postInstall = ''
+    mkdir -p $out/share/icons
+    cp ./Assets/Accessories-calculator.svg $out/share/icons/com.thekillerbunny.calculator.svg
+  '';
 }
 

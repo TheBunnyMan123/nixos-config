c5b3b072e4d98b09c177a39604ea19c61ad26a7d
TheBunnyMan123 <bunny@thekillerbunny.com>
2024-08-16 11:44:13 -0500

    move

--- hosts/common/gui/gui-home.nix
+++ hosts/common/gui/gui-home.nix
@@ -5,6 +5,209 @@
 }: {
   home-manager.users = {
     bunny = {
+      programs.vscode = {
+        enable = true;
+        enableUpdateCheck = false;
+        package = pkgs.vscodium;
+
+        extensions = with pkgs.vscode-extensions; [
+          mhutchie.git-graph
+          jnoortheen.nix-ide
+          aaron-bond.better-comments
+          pkgs.vscode-extensions.catppuccin.catppuccin-vsc-icons
+          pkgs.vscode-extensions.catppuccin.catppuccin-vsc
+        ];
+
+        userSettings = {
+          "workbench.colorTheme" = "Catppuccin Mocha";
+          "workbench.iconTheme" = "catppuccin-mocha";
+        };
+      };
+
+      programs.kitty = {
+        enable = true;
+        catppuccin = {
+          flavor = "mocha";
+          enable = true;
+        };
+        font = {
+          name = "monospace";
+          size = 11;
+        };
+        shellIntegration = {
+          enableZshIntegration = true;
+          mode = "no-rc";
+        };
+      };
+
+      programs.vencord = {
+        enable = true;
+        package = pkgs.vencord;
+        themes = {
+          catppuccin = pkgs.fetchurl {
+            url = "https://catppuccin.github.io/discord/dist/catppuccin-mocha-mauve.theme.css";
+            hash = "sha256-OaQofvUCGD50/LPu7tf3zZ9/GEktGCoeZ3J4ujtCcTE=";
+          };
+          squarecorners = pkgs.fetchurl {
+            url = "https://gist.githubusercontent.com/TheBunnyMan123/6315b2b6db6096ae8485736b4ebbceff/raw/squarecorners.theme.css";
+            hash = "sha256-sLAwWkv/7osL3oubsYldqivzFPPJMFkZCvAenv6oC1s=";
+          };
+        };
+        plugins = {
+          AutomodContext.enabled = true;
+          BetterRoleContext.enabled = true;
+          CopyEmojiMarkdown = {
+            enabled = true;
+            copyUnicode = true;
+          };
+          CopyUserURLs.enabled = true;
+          Dearrow = {
+            enabled = true;
+            hideButton = false;
+            replaceElements = 0;
+          };
+          ForceOwnerCrown.enabled = true;
+          FriendsSince.enabled = true;
+          MessageLoggerEnhanced.enabled = true;
+          ImplicitRelationships.enabled = true;
+          KeepCurrentChannel.enabled = true;
+          MessageLinkEmbeds = {
+            enabled = true;
+            listMode = "blacklist";
+            idList = "";
+            automodEmbeds = "never";
+          };
+          MoreUserTags = {
+            enabled = true;
+            tagSettings = {
+              WEBHOOK = {
+                text = "WebHook";
+                showInChat = true;
+                showInNotChat = true;
+              };
+              OWNER = {
+                text = "Owner";
+                showInChat = true;
+                showInNotChat = true;
+              };
+              ADMINISTRATOR = {
+                text = "Admin";
+                showInChat = true;
+                showInNotChat = true;
+              };
+              MODERATOR_STAFF = {
+                text = "Staff";
+                showInChat = true;
+                showInNotChat = true;
+              };
+              MODERATOR = {
+                text = "Mod";
+                showInChat = true;
+                showInNotChat = true;
+              };
+              VOICE_MODERATOR = {
+                text = "VC Mod";
+                showInChat = true;
+                showInNotChat = true;
+              };
+              CHAT_MODERATOR = {
+                text = "Chat Mod";
+                showInChat = true;
+                showInNotChat = true;
+              };
+            };
+          };
+          MutualGroupDMs.enabled = true;
+          NoOnboardingDelay.enabled = true;
+          NormalizeMessageLinks.enabled = true;
+          PictureInPicture.enabled = true;
+          PlatformIndicators = {
+            enabled = true;
+            colorMobileIndicator = true;
+            list = true;
+            badges = true;
+            messages = true;
+          };
+          RelationshipNotifier.enabled = true;
+          Summaries = {
+            enabled = true;
+            summaryExpiryThresholdDays = 3;
+          };
+          ShowHiddenThings = {
+            enabled = true;
+            showTimeouts = true;
+            showInvitesPaused = true;
+            showModView = true;
+            disableDiscoveryFilters = true;
+            disableDisallowedDiscoveryFilters = true;
+          };
+          SilentTyping = {
+            enabled = true;
+            showIcon = true;
+            contextMenu = true;
+            isEnabled = true;
+          };
+          TypingTweaks = {
+            enabled = true;
+            alternativeFormatting = true;
+            showRoleColors = true;
+            showAvatars = true;
+          };
+          ValidReply.enabled = true;
+          ValidUser.enabled = true;
+          ViewRaw.enabled = true;
+          VoiceDownload.enabled = true;
+          NoTrack = {
+            enabled = true;
+            disableAnalytics = true;
+          };
+          Settings = {
+            enabled = true;
+            settingsLocation = "aboveNitro";
+          };
+          FixSpotifyEmbeds.enabled = true;
+          FixYoutubeEmbeds.enabled = true;
+          YoutubeAdblock.enabled = true;
+          WatchTogetherAdblock.enabled = true;
+          NewPluginsManager.enabled = true;
+          NoProfileThemes.enabled = true;
+          ColorSighted.enabled = true;
+          BetterFolders = {
+            enabled = true;
+            sidebar = true;
+            sidebarAnim = true;
+            closeAllFolders = true;
+            closeAllHomeButton = true;
+            closeOthers = true;
+            forceOpen = true;
+            keepIcons = true;
+            showFolderIcon = 1;
+          };
+          PlainFolderIcon.enabled = true;
+        };
+        userPlugins = {
+          NewPluginsManager = "github:sqaaakoi/vc-newpluginsmanager/6f6fa79ea1dabaebf3c176eb1e61a4a80c6d9f97";
+          MessageLoggerEnhanced = "github:syncxv/vc-message-logger-enhanced/3fb2fe04b8e38813290309836983309a83ffe00c";
+        };
+      };
+      programs.vesktop = {
+        enable = true;
+      };
+
+      # WM
+      catppuccin.pointerCursor = {
+        flavor = "mocha";
+        accent = "mauve";
+        enable = true;
+      };
+      qt.style = {
+        name = "kvantum";
+        catppuccin = {
+          flavor = "mocha";
+          accent = "mauve";
+          enable = true;
+        };
+      };
       gtk = {
         enable = true;
         cursorTheme = {
